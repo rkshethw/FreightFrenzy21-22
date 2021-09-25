fac4901c9586239aad23b881b5c32e0a6e3fb2ad
rkshethw <rushdashethwala@gmail.com>
2021-09-25 10:53:20 -0500

    Comment added by RS to test personal login and pushing to team's repository

---
+++ TeamCode/src/main/java/StartingClass.java
@@ -0,0 +1,6 @@
+public class StartingClass {
+
+    //I am Rushda Shethwala on 9-25-21 15 10:51am
+
+    //I am adding this comment to test if my commit from my personally git hub account logged in works and pushes the commit to our official GitHub account repository
+}
